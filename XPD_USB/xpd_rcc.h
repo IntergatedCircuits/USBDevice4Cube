eb0689b27e9d8e51ccbc098ee90525c9bf195c1a
Benedek Kupper <kupper.benedek@gmail.com>
2018-11-27 20:35:35 +0100

    Fix clock control (#3)

--- XPD_USB/xpd_rcc.h
+++ XPD_USB/xpd_rcc.h
@@ -3,24 +3,19 @@
 
 #include <xpd_common.h>
 
-#define RCC_POS_USB             1
-#define RCC_POS_OTG_FS          2
-#define RCC_POS_OTG_HS          3
+#define RCC_POS_USB_EN          __HAL_RCC_USB_CLK_ENABLE
+#define RCC_POS_USB_DIS         __HAL_RCC_USB_CLK_DISABLE
+#define RCC_POS_OTG_FS_EN       __HAL_RCC_USB_OTG_FS_CLK_ENABLE
+#define RCC_POS_OTG_FS_DIS      __HAL_RCC_USB_OTG_FS_CLK_DISABLE
+#define RCC_POS_OTG_HS_EN       __HAL_RCC_USB_OTG_HS_CLK_ENABLE
+#define RCC_POS_OTG_HS_DIS      __HAL_RCC_USB_OTG_HS_CLK_DISABLE
+#define RCC_POS_USBPHYC_EN      __HAL_RCC_OTGPHYC_CLK_ENABLE
+#define RCC_POS_USBPHYC_DIS     __HAL_RCC_OTGPHYC_CLK_DISABLE
+#define RCC_POS_OTG_HS_ULPI_EN  __HAL_RCC_USB_OTG_HS_ULPI_CLK_ENABLE
+#define RCC_POS_OTG_HS_ULPI_DIS __HAL_RCC_USB_OTG_HS_ULPI_CLK_DISABLE
 
-#if defined(USB)
-#define RCC_vClockEnable(A)     __HAL_RCC_USB_CLK_ENABLE()
-#define RCC_vClockDisable(A)    __HAL_RCC_USB_CLK_DISABLE()
-#elif defined(USB_OTG_HS)
-#define RCC_vClockEnable(A)     (((A) == RCC_POS_OTG_FS) ?  \
-        __HAL_RCC_USB_OTG_FS_CLK_ENABLE() :                 \
-        __HAL_RCC_USB_OTG_HS_CLK_ENABLE())
-#define RCC_vClockEnable(A)     (((A) == RCC_POS_OTG_FS) ?  \
-        __HAL_RCC_USB_OTG_FS_CLK_DISABLE() :                \
-        __HAL_RCC_USB_OTG_HS_CLK_DISABLE())
-#elif defined(USB_OTG_FS)
-#define RCC_vClockEnable(A)     __HAL_RCC_USB_OTG_FS_CLK_ENABLE()
-#define RCC_vClockDisable(A)    __HAL_RCC_USB_OTG_FS_CLK_DISABLE()
-#endif
+#define RCC_vClockEnable(A)     A##_EN()
+#define RCC_vClockDisable(A)    A##_DIS()
 #define RCC_ulClockFreq_Hz(A)   HAL_RCC_GetHCLKFreq()
 
 #endif /* __XPD_RCC_H_ */
